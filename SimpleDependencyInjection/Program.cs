638037116bde327efc8cc870133cd59ef5585768
Rene <rene.perez.castro@gmail.com>
2018-06-16 09:37:44 -0500

    Creating simple example of dependency injection using autofact

--- SimpleDependencyInjection/Program.cs
+++ SimpleDependencyInjection/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Autofac;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,31 @@ namespace SimpleDependencyInjection
 {
 	class Program
 	{
+		private static IContainer Container { get; set; }
+
 		static void Main(string[] args)
 		{
+			var builder = new ContainerBuilder();
+			builder.RegisterType<ConsoleOutput>().As<IOutput>();
+			builder.RegisterType<TodayWriter>().As<IDateWriter>();
+			Container = builder.Build();
+
+			// The WriteDate method is where we'll make use
+			// of our dependency injection. We'll define that
+			// in a bit.
+			WriteDate();
+		}
+
+		public static void WriteDate()
+		{
+			// Create the scope, resolve your IDateWriter,
+			// use it, then dispose of the scope.
+			using (var scope = Container.BeginLifetimeScope())
+			{
+				var writer = scope.Resolve<IDateWriter>();
+				writer.WriteDate();
+				Console.ReadLine();
+			}
 		}
 	}
 }
